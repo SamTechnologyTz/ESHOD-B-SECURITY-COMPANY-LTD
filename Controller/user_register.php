ad07294f5770b99883c718b8db4f4822605b5819
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-07 23:24:22 +0300

    Update user_register.php

--- Controller/user_register.php
+++ Controller/user_register.php
@@ -18,7 +18,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       return $data;
    }
    
-   $jina = mysql_real_escape_string($unga, hakiki($data));
+   $jina = mysql_real_escape_string($unga, hakiki($data['name']));
+   $pepe = mysql_real_escape_string($unga, hakiki($data['email']));
+   $msimbo = mysql_real_escape_string($unga, password_hash($data['msimbo']));
   
    $stmt = $unga->prepare("SELECT * FROM `users` WHERE jina = ? ");
    $stmt->bind_param('s', $jina);
@@ -28,9 +30,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    if ($jina_result === 1) {
       echo json_encode(['message' => 'This username is already taken']);
       exit;
+   }
+
+   $stmt = $unga->prepare("SELECT * FROM `users` WHERE pepe = ? ");
+   $stmt->bind_param('s', $pepe);
+   $stmt->execute();
+   $email_result = $stmt->get_result();
+
+   if ($email_result === 1) {
+      echo json_encode(['message' => 'This email is already taken']);
+      exit;
    } else {
       $stmt = $unga->prepare("INSERT INTO `users` WHERE (jina, pepe, msimbo) VALUES (?, ?, ?)");
       $stmt->bind_param('sss', $jina, $pepe, $msimbo);
+
+      if ($stmt->execute()) {
+         echo json_encode(['message' => 'Registration success']);
+      }
    }
 }
 ?>
