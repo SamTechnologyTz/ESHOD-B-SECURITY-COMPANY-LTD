59dcb9788dc4b8355b686112527c23693bcc1b17
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-08 00:30:12 +0300

    Update login.php

--- users/login.php
+++ users/login.php
@@ -1,9 +1,12 @@
+<?php
+require '../config/app.php';
+?>
 <!DOCTYPE html>
 <html lang="sw">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ingia - ESHOD & B SECURITY COMPANY LIMITED</title>
+    <title>Ingia - <?php echo APP_NAME; ?></title>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
@@ -11,7 +14,7 @@
 <div class="login-container">
     <h2>Ingia kwenye Akaunti</h2>
     <p>Weka taarifa zako za kuingia ili kufikia akaunti yako.</p>
-    <form action="process_login.php" method="POST">
+    <form action="" method="POST">
         <label for="email">Barua Pepe:</label>
         <input type="email" id="email" name="email" required>
 
@@ -22,6 +25,6 @@
         <p>Hujaandikishwa? <a href="register.php">Jisajili hapa</a>.</p>
     </form>
 </div>
-
+<script scr="../application/users.js"></script>
 </body>
 </html>
