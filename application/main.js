212fd8fba6c25d443ef9b32817d90a459a5fe1d0
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-08 00:14:31 +0300

    Create main.js

---
+++ application/main.js
@@ -0,0 +1,44 @@
+//user login
+function user_login() {
+  preventDefault();
+  const pass = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  fetch('https://localhost/somahii/Controller/user_login.php', {
+    method: 'post',
+    headers: {
+      'Content-Type: application/json'
+    },
+    body: JSON.stringify( email: pass, password: password )
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    alert(data)
+  })
+  .then((error) => {
+    consolog(error)
+  })
+}
+
+//user register
+function user_register() {
+  preventDefault();
+  const jina = document.getElementById('name').value;
+  const pepe = document.getElementById('email').value;
+  const msimbo = document.getElementById('password').value;
+
+  fetch('https://localhost/somahii/Controller/user_register.php', {
+    method: 'post',
+    headers: {
+      'Content-Type: application/json'
+    },
+    body: JSON.stringify( name: jina, email: pepe, password: msimbo )
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    alert(data)
+  })
+  .then((error) => {
+    consolog(error)
+  })
+}
