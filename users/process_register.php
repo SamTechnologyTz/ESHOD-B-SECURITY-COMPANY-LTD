26554b04da3eed71b780e570a7c312d85b0f71a0
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-07 07:35:00 +0300

    Update process_register.php

--- users/process_register.php
+++ users/process_register.php
@@ -1,7 +1,10 @@
 <?php
+header(); 
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   $data = json_decode(file_get_contents('php/input'), true)
+   $data = json_decode(file_get_contents('php/input'), true);
+   
+   if (json_last_error() === JSON_ERROR_NONE) {}
 }
 ?>
