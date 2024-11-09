51cfae617f8d999b56b302029fdbdddf6f9f7035
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 21:10:55 +0300

    Update user_login.php

--- Controller/user_login.php
+++ Controller/user_login.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
 require '../config/database.php';
+require 'kichujio.php';
 if ($_SERVER['REQUEST_METHODE'] === 'POST') {
   $data = json_decode(file_get_contents('php/input'), true);
 
