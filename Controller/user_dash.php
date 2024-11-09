cf7f569060f8ae4b7aa2c5e7153a085c49d959af
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 20:13:12 +0300

    Update user_dash.php

--- Controller/user_dash.php
+++ Controller/user_dash.php
@@ -1,5 +1,10 @@
 <?php
 header('Content-Type: application/json');
+require '../config/database.php';
 
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $data = json_decode(file_get_contents('php/input'), true);
+  $uid = mysql_realy_escape_string($unga, hakiki);
+  if () {}
+}
 ?>
