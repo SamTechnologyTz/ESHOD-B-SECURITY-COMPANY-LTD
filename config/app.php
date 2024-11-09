0303b6fc403284c063a134b601907c06cbf22b5b
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 23:41:40 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php 
-define('APP_NAME', 'BENJA');
+define('APP_NAME', 'Eshod security');
 define('EMAIL', 'admin@basanzietech.space');
 define('PHONE_NUMBER', '0657779003');
+define('DEV', 'Backend developed by SAM OCHU');
 ?>
