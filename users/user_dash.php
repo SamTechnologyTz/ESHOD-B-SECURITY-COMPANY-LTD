55f2b7fb51c91df1bf998b5bd23a226a695381e2
Benjamini Omary <167584894+basanzietech@users.noreply.github.com>
2024-11-10 00:16:11 +0300

    Update user_dash.php

--- users/user_dash.php
+++ users/user_dash.php
@@ -8,7 +8,7 @@ require '../config/app.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - <?php echo APP_NAME; ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/userform.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 <body>
