c86e9c4bb76ca2ad2f02ea5d2e0a0f3f2aca16e0
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 07:54:08 +0300

    Update user_dash.js

--- application/user_dash.js
+++ application/user_dash.js
@@ -1,1 +1,18 @@
+setInterval(() => {
+  const jina = document.getElementById('jina').value;
+  fetch('http:localhost/somahii/Controller/user_dash.php', {
+    method: 'POST',
+    header: {
+      'Content-Type: application'
+    },
+    body: JSON.stringify( name: jina )
+  })
+  .then((response) = response.json())
+  .then((data) => {
+    alert(JSON.stringify(data));
+  })
+  .then((error) => {
+    conso.log
+  })
 
+}, 500)
