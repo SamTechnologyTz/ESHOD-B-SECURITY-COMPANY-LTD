5e1d837f69eef74921dd4b0481b09c356429c50d
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 20:48:14 +0300

    Update user_dash.php

--- Controller/user_dash.php
+++ Controller/user_dash.php
@@ -4,10 +4,8 @@ require '../config/database.php';
 require 'chujio.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $taarifa = json_decode(file_get_contents('php/input'), true);
-  $safisha = new hakikisha();
-  
-  $uid = mysql_realy_escape_string($unga, $safisha->hakiki($taarifa['uid']));
-  if () {}
+    $taarifa = json_decode(file_get_contents('php://input'), true);
+    $safisha = new Hikisha($taarifa['uid']);
+    $uid = mysqli_real_escape_string($unga, $safisha->hakiki());
 }
 ?>
