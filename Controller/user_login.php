10ca194d1a47f3654a9ca4e6921fcc886caadea5
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-09 21:22:51 +0300

    Update user_login.php

--- Controller/user_login.php
+++ Controller/user_login.php
@@ -1,16 +1,18 @@
 <?php
 header('Content-Type: application/json');
 require '../config/database.php';
-require 'kichujio.php';
+require 'chujio.php';
 if ($_SERVER['REQUEST_METHODE'] === 'POST') {
-  $data = json_decode(file_get_contents('php/input'), true);
-
+  $data = json_decode(file_get_contents('php:/input'), true);
+  $emal= new Hakikisha($data['email']);
+  $pass = new Hakikisha($data['password']);
+  
   if (json_last_error() === JSON_ERROR_NONE) {
     echo json_encode(['message' => 'Error incorrect formart']);
     exit;
   }
 
-  $pepe = mysql_real_escape_string($unga, );
-  if ()
+  $pepe = mysqli_real_escape_string($unga, $emal->hakiki());
+  $msimbo = mysqli_real_escape_string($unga, $pass->hakiki());
 }
 ?>
