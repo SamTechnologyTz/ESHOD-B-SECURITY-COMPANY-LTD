f0141d8b99de80a2acd41f5d42491e970af959c9
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-12-31 12:11:03 +0300

    Delete config directory

--- config/app.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('APP_NAME', 'Eshod security');
-define('EMAIL', 'admin@basanzietech.space');
-define('PHONE_NUMBER', '0657779003');
-define('DEV', 'Backend developed by SAM OCHU');
-?>
--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$unga = new mysqli('localhost', 'root', '', 'eshod');
-
-if (!$unga) {
-  die(json_encode('message' => 'no database connection'));
-}
-?>
